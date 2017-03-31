554e1f4f8a277321e9e518251957b151681c20a6
Anders Opstrup <aopstrup@gmail.com>
2017-03-31 23:26:26 +0200

    Finished first iteration of box class

--- communicationclient/Box.java
+++ communicationclient/Box.java
@@ -1,3 +1,32 @@
+package communicationclient;
+
 public class Box {
+	private String _color;
+	private char _letter;
+	private int _x;
+	private int _y;
+
+	public Box(String color, char letter, int x, int y) {
+		_color = color;
+		_letter = letter;
+		_x = x;
+		_y = y;
+	}
+
+	public String getColor() {
+		return _color;
+	}
+
+	public char getLetter() {
+		return _letter
+	}
+
+	public int getXCoordinate() {
+		return _x;
+	}
+
+	public int getYCoordinate() {
+		return _y;
+	}
 
 }
